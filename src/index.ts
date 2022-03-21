545cb7984c52bb8c78b8f9265af918b36e1456eb
Joao Luacas <joaolucas55br@gmail.com>
2022-03-21 01:47:00 -0300

    Documentation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
-
+import swaggerFile from "./swagger.json";
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use("/users", usersRoutes);
 
 export { app };
